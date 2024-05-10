4d369c16ff76be9a25f0894606ee842134569a48
Anwarul07 <mdanwarul064@gmail.com>
2024-05-10 18:59:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("")
+
+console.log("Select menu");
+
+let SelectFiled = document.getElementsByClassName("selectfiled");
+let SelectText = document.querySelector("#selectText");
+let options = document.getElementsByClassName("options");
+let list = document.getElementById("list");
+
+
+
+SelectFiled.onclick = function () {
+   list.classList.toggle("hide");
+
+};
+
+for (option of options) {
+   option.onclick = function () {
+      SelectText.innerHTML = this.textContent;
+      list.classList.toggle("swipe");
+      console.log("wrong....");
+
+   };
+};
+
+
